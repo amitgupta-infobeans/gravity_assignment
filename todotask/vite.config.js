22fbaf650842f8dde09a67672507629298dbc673
amitkc.gupta <amitkc.gupta@infobeans.com>
2025-08-03 21:12:36 +0530

    setup for git page deploy.

--- todotask/vite.config.js
+++ todotask/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/gravity_assignment/todotask/', // 👈 replace with your GitHub repo name
 });
